5acbf49a90d665b881deb91a605fdb945a193565
Swagnar <46372739+Swagnar@users.noreply.github.com>
2022-09-30 11:16:25 +0200

    Delete sklep directory

--- sklep/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$root = pathinfo($_SERVER['SCRIPT_FILENAME']);
-define('PROJECT_NAME', 'sklep');
-define('BASE_FOLDER', basename($root['dirname']));
-define('SITE_ROOT', realpath(dirname(__FILE__)));
-?>
--- sklep/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include_once('config.php');
-    include('templates/header.php');
-    include('templates/footer.html');
-?>
